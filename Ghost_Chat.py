626170fc61bde032d5e922a2c5695dc707a238af
farros <31735132+farrosmufid@users.noreply.github.com>
2024-10-04 07:11:36 +1000

    Added logic so that the assistant asks first

--- Ghost_Chat.py
+++ Ghost_Chat.py
@@ -1,13 +1,28 @@
 import streamlit as st
 import ollama
 import tempfile
+import time
 
 # App title
 st.title("Ghost AI 🗿🗿🗿")
 
+# Initialise message
+init_msg = """Hey there! **Ghost AI** 🗿🗿🗿 here, your **handy recruitment assistant**. 
+Got any **questions** about your **application progress**?\n\nNo worries! **Just ask!**\n\n
+Feel free to mention your **name**, the **position** you applied for, 
+and the **company**—or we can just have a **casual chat**.\n\nWhether you need 
+**updates**, **info**, or **someone** to **keep you company** during the wait, 
+I’m here to help!"""
+
 # First time load messages is empty
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+# Function to stream the assistant first
+def stream_first(stream):
+    for chunk in stream:
+        yield chunk + " "
+        time.sleep(0.09)
 
 # Display chat history
 for message in st.session_state.messages:
@@ -48,3 +63,12 @@ if prompt := st.chat_input("What is up?"):
 
     # Append response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+# Assistant asks first
+if not st.session_state.messages:
+    st.session_state.messages.append({"role": "assistant", "content": init_msg})
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write_stream(stream_first(message["content"].split(" ")))
+
