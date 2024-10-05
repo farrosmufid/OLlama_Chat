91fad31f97ce7bb27115f9c8ffcd662015482296
farros <31735132+farrosmufid@users.noreply.github.com>
2024-10-05 23:02:08 +1000

    Updated the code so the assistant asks first

--- Linguo_Bot.py
+++ Linguo_Bot.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import ollama
 import tempfile
+import time
 
 # App title
 st.title("Linguo Bot")
 st.image('linguo_img.webp', width = 200, caption='Linguo Bot: Still under development')
 
+init_msg = """
+Hi, I'm Linguo Bot, a grammar-checking robot inspired by The Simpsons. 
+I'm an expert in checking grammar. 
+Send me a sentence you'd like me to fix!"
+"""
+
+# Function to stream the assistant first
+def stream_first(stream):
+    for chunk in stream:
+        yield chunk + " "
+        time.sleep(0.09)
 
 # First time load messages is empty
 if "messages" not in st.session_state:
@@ -50,3 +62,11 @@ if prompt := st.chat_input("What is up?"):
 
     # Append response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+# Assistant asks first
+if not st.session_state.messages:
+    st.session_state.messages.append({"role": "assistant", "content": init_msg})
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write_stream(stream_first(message["content"].split(" ")))
