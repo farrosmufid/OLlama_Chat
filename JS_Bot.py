6d2ead54cd2b029bcba7f2e4e578e4310e715e0b
farros <31735132+farrosmufid@users.noreply.github.com>
2024-10-06 01:03:19 +1000

    Added JS Bot

---
+++ JS_Bot.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import ollama
+import tempfile
+
+# App title
+st.title("JS Bot")
+
+# First time load messages is empty
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Prompt for user input
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Display user message
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Get response from the assistant
+    with st.chat_message("assistant"):
+        # response = ollama.chat(
+        #     model='llama3.1',
+        #     messages=[
+        #     {
+        #         "role": "user", "content": prompt
+        #      },
+        # ])
+
+        stream = ollama.chat(
+            model='js-ai',
+            messages=[{'role': 'user', 'content': prompt}],
+            stream=True,
+        )
+
+        def stream_data():
+            for chunk in stream:
+                yield chunk['message']['content']
+
+        response = st.write_stream(stream_data)
+
+        #st.markdown(result)
+
+    # Append response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
